9c86f98fb5a03c1942e3a6dbeb48d30f42440b48
rrienton <rrienton@gmail.com>
2014-04-08 17:00:04 -0500

    updated map with tiles and started map panel

---
+++ Dungeon-RPG/src/dungeonRPGPackage/MapPanel.java
@@ -0,0 +1,12 @@
+package dungeonRPGPackage;
+
+import javax.swing.JPanel;
+
+/**
+ * Object representing the GUI panel of a Map object.
+ * @author rrienton
+ *
+ */
+public class MapPanel extends JPanel {
+	
+}
