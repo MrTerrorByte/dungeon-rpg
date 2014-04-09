1696fa00bea28f16ecc76d2357ec46c587623a67
rrienton <rrienton@gmail.com>
2014-04-09 00:26:39 -0500

    practicing with images

--- Dungeon-RPG/src/dungeonRPGPackage/MapPanel.java
+++ Dungeon-RPG/src/dungeonRPGPackage/MapPanel.java
@@ -1,5 +1,9 @@
 package dungeonRPGPackage;
 
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -8,5 +12,36 @@ import javax.swing.JPanel;
  *
  */
 public class MapPanel extends JPanel {
+	private Map mapObject;		//object representation for the Map GUI
 	
+	/**
+	 * Constructs a MapPanel.
+	 * @param mapObject
+	 */
+	public MapPanel(Map mapObject){
+		this.mapObject = mapObject;
+		this.setLayout(new GridLayout(100,100));
+		ImageIcon icon = createImageIcon("images/cave.png");
+		JLabel background = new JLabel(icon);
+		this.add(background);
+	}
+	
+	/**
+	 * Returns an ImageIcon, or null if path was invalid.
+	 * (Citing:http://docs.oracle
+	 * .com/javase/tutorial/uiswing/components/icon.html)
+	 * 
+	 * @param path
+	 *            path where image is located
+	 * @return ImageIcon with our image, or null
+	 */
+	private ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = getClass().getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
 }
