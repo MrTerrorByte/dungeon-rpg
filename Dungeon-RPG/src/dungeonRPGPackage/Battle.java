d5a826c9af993a636ad1ce35dbbe14c595c69199
MrTerrorByte <jordanh225@yahoo.com>
2014-04-09 23:49:49 -0500

    reset..

--- Dungeon-RPG/src/dungeonRPGPackage/Battle.java
+++ Dungeon-RPG/src/dungeonRPGPackage/Battle.java
@@ -11,9 +11,27 @@ public class Battle {
 	 * @return : 0 on success, -1 on failure
 	 */
 	public static int attack(Hero hero, Monster monster, boolean Turn){
-		if(Turn){
-			
+		Weapon heroWeapon = hero.getWeapon();
+		if(Turn){ //hero's turn
+			monster.setCurrHealth(monster.getCurrHealth()- heroWeapon.getAttack());
+			if(monster.getCurrHealth() <= 0){
+				endBattle("hero", hero, monster);
+			}
+			Turn = false;
+		}
+		else{ //monster's turn
+			hero.setCurrHealth(hero.getCurrHealth() - monster.getAttack());
+			if(hero.getCurrHealth() <= 0){
+				endBattle("monster", hero, monster);
+			}
 		}
 		return 0;
+	}
+	
+	public static void endBattle(String victor, Hero hero, Monster monster){
+		if(victor.equals("hero")){ //if the victor is hero, then have its weapon gain exp
+			hero.getWeapon().gainExp(monster.getExpGain());
+			hero.setGold(hero.getGold() + monster.getGoldGain());
+		}
 	}
 }
