2281fe82bdca7fab3e09174fc3fb44746380a612
MrTerrorByte <jordanh225@yahoo.com>
2014-04-16 17:38:57 -0500

    changed JFrame format to use JPanel + paint()

--- Dungeon-RPG/src/dungeonRPGPackage/GameController.java
+++ Dungeon-RPG/src/dungeonRPGPackage/GameController.java
@@ -1,6 +1,7 @@
 package dungeonRPGPackage;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -16,6 +17,7 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import dungeonRPGPackage.Map.Tile;
 
@@ -28,6 +30,8 @@ public class GameController implements KeyListener{
 	private BufferStrategy buffer;
 	private Hero hero;
 	private int tileSize = Map.FRAMEWIDTH/Map.ARRAYSIZE;
+	private JFrame dungeonFrame = new JFrame();
+    private StartPanel dungeonPanel = new StartPanel();
 	//private Map dungeonMaps[];
     
     /**
@@ -40,17 +44,17 @@ public class GameController implements KeyListener{
     	Weapon weapon = new Weapon("Sword","Sword",0,0);
     	Shield shield = new Shield("Shield","Shield",0,0);
     	hero = new Hero("Hero", dungeonMap, weapon, shield);
-    	
-        JFrame dungeonFrame = new JFrame();
         
-        dungeonFrame.setTitle("Dungeon-RPG");
-        dungeonFrame.setLayout(null);
+        dungeonPanel.setName("Dungeon-RPG");
+        dungeonPanel.setLayout(null);
+        
         dungeonFrame.setResizable(false);
+        
         //dungeonFrame.setUndecorated(true);
-        dungeonFrame.setBounds(0, 0, Map.FRAMEWIDTH, Map.FRAMEHEIGHT);
-        dungeonFrame.setLocationRelativeTo(null);
-        dungeonFrame.setVisible(true);
-        dungeonFrame.addKeyListener(this);
+        dungeonPanel.setBounds(0, 0, Map.FRAMEWIDTH, Map.FRAMEHEIGHT);
+        //dungeonPanel.setLocationRelativeTo(null);
+        dungeonPanel.setVisible(true);
+        dungeonPanel.addKeyListener(this);
 
 		dungeonFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -72,69 +76,18 @@ public class GameController implements KeyListener{
             }
         }
 
-        dungeonFrame.createBufferStrategy(2);
-        buffer = dungeonFrame.getBufferStrategy();
-
-        gameLoop();
+        //dungeonFrame.createBufferStrategy(2);
+        //buffer = dungeonFrame.getBufferStrategy();
+        
+        dungeonFrame.add(dungeonPanel);
+        dungeonFrame.setSize(Map.FRAMEWIDTH, Map.FRAMEHEIGHT);
+		dungeonFrame.setResizable(false);
+		dungeonFrame.setVisible(true);
+        dungeonFrame.validate();
+        dungeonPanel.requestFocus();
+        //gameLoop();
     }
 
-    private void gameLoop()
-    {
-        while (true) {
-            Graphics2D tileGraphics = (Graphics2D) buffer.getDrawGraphics();
-            BufferedImage heroImage = null;
-            BufferedImage grassImage = null;
-            BufferedImage caveImage = null;
-            try {
-                heroImage = ImageIO.read(new File("src/images/maleBackStanding.png"));
-            } catch (IOException e) {
-            	System.out.println("Hero image Doesnt exist");
-            }
-            try {
-                grassImage = ImageIO.read(new File("src/images/grass.png"));
-            } catch (IOException e) {
-            	System.out.println("Grass image Doesnt exist");
-            }
-            try {
-                caveImage = ImageIO.read(new File("src/images/cave.png"));
-            } catch (IOException e) {
-            	System.out.println("Cave image Doesnt exist");
-            }
-            
-            for (int row = 0; row < Map.ARRAYSIZE; row++) {
-                for (int col = 0; col < Map.ARRAYSIZE; col++) {
-                    switch (hero.getMap().getTileArray()[row][col]) {
-                        case GRASS:
-                            tileGraphics.setColor(Color.GREEN);
-                            tileGraphics.drawImage(grassImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
-                            break;
-                        case ROCK:
-                            //tileGraphics.setColor(Color.GRAY);
-                            //tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
-                        	tileGraphics.drawImage(caveImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
-                        	break;
-                        case NONE:
-                        	tileGraphics.setColor(Color.BLACK);
-                        	tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
-                        	break;
-                    }
-                    //tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
-                }
-            }
-            //tileGraphics.setColor(Color.RED);
-            //tileGraphics.fillRect(hero.getX()*tileSize, hero.getY()*tileSize, tileSize, tileSize);
-			tileGraphics.drawImage(heroImage, hero.getX()*tileSize, hero.getY()*tileSize, null);
-            
-            buffer.show();
-            tileGraphics.dispose();
-            
-            try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-        }
-    }
     
     /*private void generateMaps(){
     	for(int mapNumber = 0; mapNumber < 10; mapNumber++){
@@ -156,18 +109,22 @@ public class GameController implements KeyListener{
 		//press up key
 		if(arg0.getKeyCode() == KeyEvent.VK_UP){
 			hero.move(hero.getX(), hero.getY()-1);
+			dungeonPanel.repaint();
 		}
 		//press down key
 		else if(arg0.getKeyCode() == KeyEvent.VK_DOWN){
 			hero.move(hero.getX(), hero.getY()+1);
+			dungeonPanel.repaint();
 		}
 		//press left key
 		else if(arg0.getKeyCode() == KeyEvent.VK_LEFT){
 			hero.move(hero.getX()-1, hero.getY());
+			dungeonPanel.repaint();
 		}
 		//press right key
 		else if(arg0.getKeyCode() == KeyEvent.VK_RIGHT){
 			hero.move(hero.getX()+1, hero.getY());
+			dungeonPanel.repaint();
 		}
 		//press esc
 		else if(arg0.getKeyCode() == KeyEvent.VK_ESCAPE){
@@ -187,4 +144,49 @@ public class GameController implements KeyListener{
 		
 	}
 
+	private class StartPanel extends JPanel{
+		@Override
+		public void paint(Graphics g){
+			BufferedImage heroImage = null;
+            BufferedImage grassImage = null;
+            BufferedImage caveImage = null;
+            try {
+                heroImage = ImageIO.read(new File("src/images/maleBackStanding.png"));
+            } catch (IOException e) {
+            	System.out.println("Hero image Doesnt exist");
+            }
+            try {
+                grassImage = ImageIO.read(new File("src/images/grass.png"));
+            } catch (IOException e) {
+            	System.out.println("Grass image Doesnt exist");
+            }
+            try {
+                caveImage = ImageIO.read(new File("src/images/cave.png"));
+            } catch (IOException e) {
+            	System.out.println("Cave image Doesnt exist");
+            }
+            
+            Graphics2D tileGraphics = (Graphics2D) g;
+            
+            for (int row = 0; row < Map.ARRAYSIZE; row++) {
+                for (int col = 0; col < Map.ARRAYSIZE; col++) {
+                    switch (hero.getMap().getTileArray()[row][col]) {
+                        case GRASS:
+                            tileGraphics.setColor(Color.GREEN);
+                            tileGraphics.drawImage(grassImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
+                            break;
+                        case ROCK:
+                        	tileGraphics.drawImage(caveImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
+                        	break;
+                        case NONE:
+                        	tileGraphics.setColor(Color.BLACK);
+                        	tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
+                        	break;
+                    }
+                }
+            }
+			tileGraphics.drawImage(heroImage, hero.getX()*tileSize, hero.getY()*tileSize, null);
+            
+		}
+	}
 }
