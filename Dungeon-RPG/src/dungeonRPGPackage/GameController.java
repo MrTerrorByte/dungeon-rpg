e00751054b78e479677a9b7620488f74e5d0a693
MrTerrorByte <jordanh225@yahoo.com>
2014-04-16 09:15:19 -0500

    added sleep, so it runs smoother on my machine.

--- Dungeon-RPG/src/dungeonRPGPackage/GameController.java
+++ Dungeon-RPG/src/dungeonRPGPackage/GameController.java
@@ -94,7 +94,12 @@ public class GameController implements KeyListener{
             }
             tileGraphics.setColor(Color.RED);
             tileGraphics.fillRect(hero.getX()*tileSize, hero.getY()*tileSize, tileSize, tileSize);
-			
+            
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
             buffer.show();
             tileGraphics.dispose();
         }
