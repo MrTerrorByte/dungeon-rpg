86633948948ec6b1c095e3a277a9f00c39071311
rrienton <rrienton@gmail.com>
2014-04-16 16:53:13 -0500

    changed cave and grass and hero

--- Dungeon-RPG/src/dungeonRPGPackage/GameController.java
+++ Dungeon-RPG/src/dungeonRPGPackage/GameController.java
@@ -63,7 +63,7 @@ public class GameController implements KeyListener{
             	if(col > 5){
             		hero.getMap().getTileArray()[row][col] = Tile.GRASS;
             	}
-            	else if(row > 3 && row < 8 && col > 2){
+            	else if(row > 4 && row < 7 && col > 3){
             		hero.getMap().getTileArray()[row][col] = Tile.NONE;
             	}
             	else{
@@ -84,15 +84,21 @@ public class GameController implements KeyListener{
             Graphics2D tileGraphics = (Graphics2D) buffer.getDrawGraphics();
             BufferedImage heroImage = null;
             BufferedImage grassImage = null;
+            BufferedImage caveImage = null;
             try {
-                heroImage = ImageIO.read(new File("src/images/male hero.png"));
+                heroImage = ImageIO.read(new File("src/images/maleBackStanding.png"));
             } catch (IOException e) {
             	System.out.println("Hero image Doesnt exist");
             }
             try {
                 grassImage = ImageIO.read(new File("src/images/grass.png"));
             } catch (IOException e) {
-            	System.out.println("Hero image Doesnt exist");
+            	System.out.println("Grass image Doesnt exist");
+            }
+            try {
+                caveImage = ImageIO.read(new File("src/images/cave.png"));
+            } catch (IOException e) {
+            	System.out.println("Cave image Doesnt exist");
             }
             
             for (int row = 0; row < Map.ARRAYSIZE; row++) {
@@ -103,9 +109,10 @@ public class GameController implements KeyListener{
                             tileGraphics.drawImage(grassImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
                             break;
                         case ROCK:
-                            tileGraphics.setColor(Color.GRAY);
-                            tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
-                            break;
+                            //tileGraphics.setColor(Color.GRAY);
+                            //tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
+                        	tileGraphics.drawImage(caveImage, row*tileSize, col*tileSize, tileSize, tileSize, null);
+                        	break;
                         case NONE:
                         	tileGraphics.setColor(Color.BLACK);
                         	tileGraphics.fillRect(row*tileSize, col*tileSize, tileSize, tileSize);
